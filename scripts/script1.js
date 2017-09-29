27ab45d0e7533324c34e9766d676c9b3aee9b72e
Evan Miller <eamiller@dhcp-asv-100.helsinki.fi>
2017-09-29 16:42:20 +0300

    smth

--- scripts/script1.js
+++ scripts/script1.js
@@ -71,3 +71,21 @@ window.onclick = function(event) {
         }
     }
 }
+function psyFunction() {
+    document.getElementById("myPsyDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.psy-dropbtn')) {
+
+        var dropdowns = document.getElementsByClassName("psy-dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
